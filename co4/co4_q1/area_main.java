98a5d1cba1edbb569a2034e2a9860b991c23c508
amal s <2017@tkmce.ac.in>
2021-06-03 18:02:25 +0530

    co4

---
+++ co4/co4_q1/area_main.java
@@ -0,0 +1,22 @@
+package co4;
+
+import co4.rectangle;
+public class area_main {
+
+	public static void main(String[] args) {
+		rectangle obj=new rectangle();
+		obj.input();
+		obj.area();
+		triangle obj1=new triangle();
+		obj1.input();
+		obj1.area();
+	    square obj2=new square();
+	    obj2.input();
+		obj2.area();
+		circle obj3=new circle();
+		obj3.input();
+		obj3.area();
+
+	}
+
+}
