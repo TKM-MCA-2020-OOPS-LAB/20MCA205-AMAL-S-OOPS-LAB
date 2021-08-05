8e836af27565b4cbf12b1758161695fa6832cc20
amal s <2017@tkmce.ac.in>
2021-08-05 21:57:24 +0530

    java

---
+++ co4/co4_q11/co4_q11.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class co4_q11 {
+	public static void main(String[] args) {
+	   LinkedList<String> list=new LinkedList<String>();
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter number of employees:");
+	   int num=sc.nextInt();
+	   for(int i=1;i<=num;i++){
+        System.out.print("Add the name of employee id="+i+":");
+        String s=sc.next();
+        list.add(s);
+        }
+      System.out.println();
+      System.out.println("LinkedList After adding employees:");
+      Iterator<String> itr=list.iterator();
+      while(itr.hasNext()){
+        System.out.println(itr.next());
+      }
+     list.clear();
+     System.out.println("Linked list After removing employees: "+list);
+  }
+}
