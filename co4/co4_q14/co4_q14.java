0f0dbc5a04d3bcc81e01743972623e753870f42a
Amal-2020 <75247206+Amal-2020@users.noreply.github.com>
2021-08-15 21:39:55 +0530

    q14

---
+++ co4/co4_q14/co4_q14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Deque; 
+public class co4_q14{  
+    public static void main(String args[]) {
+    	Deque<String> deqe = new ArrayDeque<String>();  
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of employees:");
+ 	    int num=sc.nextInt();
+ 	    for(int i=1;i<=num;i++){
+        System.out.print("Add the name of employee id="+i+"\n");
+        String s=sc.next();
+        deqe.add(s);
+ 	    }
+ 	   System.out.println("\nInitial list of employee after addition:"+deqe);
+		System.out.println("\nAfter removing all employees:");
+		for(int i =0;i<num;i++)
+		{
+				deqe.pop();
+	 	}
+		System.out.println("\nFinal list of employees:"+deqe);
+    }
+}
