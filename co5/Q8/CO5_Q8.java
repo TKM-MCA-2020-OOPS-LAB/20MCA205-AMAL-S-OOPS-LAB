c62bacbe0cf30ca66c82ef711a27c7d1a9e258db
Amal-2020 <75247206+Amal-2020@users.noreply.github.com>
2021-09-09 13:05:54 +0530

    co5_q8

---
+++ co5/Q8/CO5_Q8.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+/*
+<applet code="CO5_Q8" width=200 height=300>
+</applet>
+*/
+public class CO5_Q8 extends Applet implements KeyListener{
+   TextField t;
+   TextArea tA;
+public void init(){
+   t=new TextField(20);
+   tA=new TextArea();
+   add(t);
+   add(tA);
+   t.addKeyListener(this);
+  }
+public void keyPressed(KeyEvent e){
+   if(e.getSource()==t)
+   tA.append("keyPressed\n");
+  }
+public void keyReleased(KeyEvent e){
+   if(e.getSource()==t)
+   tA.append("Key Released\n");
+  }
+public void keyTyped(KeyEvent e){
+   if(e.getSource()==t){
+   char c=e.getKeyChar();
+   tA.append("KeyTyped\n");
+  }
+ }
+}
