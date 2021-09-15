7e9f3e94c1559e918001980d2ce31a781856c786
Amal-2020 <75247206+Amal-2020@users.noreply.github.com>
2021-09-15 22:52:11 +0530

    co6_q3

--- co6/co6_q1/co6_q3/co6_q3.java
+++ co6/co6_q1/co6_q3/co6_q3.java
@@ -1,14 +1,14 @@
 package co6;
 import java.io.*;  
-class co6_q3 {
+public class co6_q3 {
 	
-	public static void main(String[] args) throws IOException{
-		    FileReader fr = new FileReader("input.txt");
-			FileWriter fw = new FileWriter("output.txt", true);
+	public static void main(String[] args)throws IOException{
+		FileReader fr = new FileReader("C:\\Users\\ACER\\Desktop\\git\\20MCA205-AMAL-S-OOPS-LAB\\co6\\co6_q3\\in.txt");
+			FileWriter fw = new FileWriter("out.txt");
              System.out.println("file copied");
-			 int character;
-             while ((character = fr.read()) != -1) {
-            	 fw.write(character); 
+			 int ch;
+             while ((ch = fr.read()) != -1) {
+            	 fw.write(ch); 
              }
              fr.close();
              fw.close();
